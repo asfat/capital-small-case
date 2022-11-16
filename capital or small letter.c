79eb57aa57b03492dcd36bbaeae3bdd577df2b41
Rahat <01rahat01@gmail.com>
2022-11-16 07:44:51 +0600

    first commit

---
+++ capital or small letter.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c",&ch);
+    if(ch>='A' && ch<='Z')
+        printf("Capital Letter");
+    else if(ch>='a' && ch<='z')
+        printf("Small Letter");
+    else
+        printf("Not a letter");
+    return 0;
+}
